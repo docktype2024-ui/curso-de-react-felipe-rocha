f86209d8c815f640a2a9046d4e6dd118379f8a92
dockie <docktype2024@gmail.com>
2025-11-06 10:17:31 +0000

    atualizacao

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,85 @@
-
-import Tasks from "./components/Tasks"
-import AddTask from "./components/AddTask"
-import './App.css'
-import { useState } from "react"
-import {v4} from 'uuid'
-
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
+import "./App.css";
+import { useEffect, useState } from "react";
+import { v4 } from "uuid";
 
 function App() {
-  const [tasks,setTasks] = useState([{
-    id:1,
-    title:'Estudar programacao',
-    description: 'Estudar programcao para se tornar um desenvolvedor fullstack',
-    isCompleted:false,
-  },
-  {
-    id:2,
-    title:'Estudar ingles',
-    description: 'Estudar ingles para se tornar fluente',
-    isCompleted:false,
-  },
-  {
-    id:3,
-    title:'Estudar matematica',
-    description: 'Estudar matematica para se tornar fluente',
-    isCompleted:false,
-  },
-])
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem("tasks")) || []
+  );
 
-function onTaskClick(taskId){
-  const newTasks = tasks.map(task => {
-    //Preciso atualizar a tarefa
-    if (task.id === taskId){
-      return { ...task, isCompleted: !task.isCompleted }
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  useEffect(() => {
+    //Chamar api
+    const fetchTasks = async () => {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos?_limit=10",
+        {
+          method: "GET",
+        }
+      );
+      const data = await response.json();
+      
+
+      //Pegar os dados que ela retorna
+      console.log(data);
+      //Armazenar esses dados no state
+      
+      //Se eu quiser posso pegar uma api para pegar as tarefas
+      //setTasks(data)
     }
+    fetchTasks()
 
-    //Nao preciso atualizar a tarefa
-    return task
-  })
+  }, []);
 
-  setTasks(newTasks)
-}
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+      //Preciso atualizar a tarefa
+      if (task.id === taskId) {
+        return { ...task, isCompleted: !task.isCompleted };
+      }
 
-function deleteTaskClick(taskId){
-    const newTasks = tasks.filter(task => task.id !== taskId)
-    setTasks(newTasks)
-}
+      //Nao preciso atualizar a tarefa
+      return task;
+    });
 
-function onTaskSubmit(title,description){
-  const newTask = {
-    id: v4(),
-    title: title,
-    description:description,
-    isCompleted:false,
+    setTasks(newTasks);
   }
-  setTasks([...tasks , newTask])
-}
+
+  function deleteTaskClick(taskId) {
+    const newTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(newTasks);
+  }
+
+  function onTaskSubmit(title, description) {
+    const newTask = {
+      id: v4(),
+      title: title,
+      description: description,
+      isCompleted: false,
+    };
+    setTasks([...tasks, newTask]);
+  }
 
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
       <div className="w-[500px] space-y-4">
-        <h1 className="text-3xl text-slate-100 font-bold text-center">Gerenciador de tarefas</h1>
-        <AddTask onTaskSubmit={onTaskSubmit}/>
-        <Tasks tasks={tasks} onTaskClick={onTaskClick} deleteTaskClick={deleteTaskClick}/>
-      </div>   
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de tarefas
+        </h1>
+        <AddTask onTaskSubmit={onTaskSubmit} />
+        <Tasks
+          tasks={tasks}
+          onTaskClick={onTaskClick}
+          deleteTaskClick={deleteTaskClick}
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
